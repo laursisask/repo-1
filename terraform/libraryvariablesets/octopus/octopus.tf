3259b8b713d074417fb55563862bda4d2ac8702e
Matthew Casperson <matthewcasperson@gmail.com>
2023-04-21 07:09:45 +1000

    Shortened some lines of text

--- terraform/libraryvariablesets/octopus/octopus.tf
+++ terraform/libraryvariablesets/octopus/octopus.tf
@@ -16,14 +16,14 @@ variable "octopus_server" {
   type        = string
   nullable    = false
   sensitive   = false
-  description = "The URL of the Octopus server e.g. https://myinstance.octopus.app."
+  description = "The URL of the Octopus server"
 }
 
 variable "octopus_apikey" {
   type        = string
   nullable    = false
   sensitive   = true
-  description = "The API key used to access the Octopus server. See https://octopus.com/docs/octopus-rest-api/how-to-create-an-api-key for details on creating an API key."
+  description = "The API key used to access the Octopus server."
 }
 
 variable "octopus_space_id" {
